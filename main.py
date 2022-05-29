c45a2b632f920ede8e9e06580f2fed95f82fe972
Unknown-0perator <ahmadrashidakhtar@gmail.com>
2022-05-28 20:07:10 -0700

    spotify connected

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
-
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 CLIENT_ID = 'Your Spotify Client_ID'
 CLIENT_SECRET = 'Your Spotify Client_Secret'
@@ -13,3 +14,15 @@ billboard = response.text
 soup = BeautifulSoup(billboard, 'html.parser')
 song_list = [song.getText().strip() for song in soup.findAll(name='h3', class_='a-no-trucate')]
 print(song_list)
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope='playlist-modify-private',
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri='Your redirect_uri from Spotify',
+        show_dialog=True,
+        cache_path='Token.txt'
+    )
+)
+user_id = sp.current_user()['id']
